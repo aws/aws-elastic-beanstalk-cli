896536e2e1e3500e853b4030d81075ac0f5d62e5
Nick <humrichn@amazon.com>
2015-02-06 16:10:07 -0800

    Fix setup script for py2exe
    cr https://cr.amazon.com/r/3331228/

--- setup.py
+++ setup.py
@@ -60,16 +60,59 @@ setup_options = dict(
 )
 
 if 'py2exe' in sys.argv:
+    data_files = setup_options['package_data']
     # This will actually give us a py2exe command.
     import py2exe
+    import cement.ext
+    import pkgutil
+    import encodings
+    # We need to manually include all cement.ext modules since py2exe doesnt
+    # pull them in.
+    includes = []
+    for importer, modname, ispkg in pkgutil.iter_modules(cement.ext.__path__):
+        includes.append('cement.ext.' + modname)
     # And we have some py2exe specific options.
     setup_options['options'] = {
         'py2exe': {
+            'includes': ['encodings'] + includes,
             'optimize': 0,
             'skip_archive': True,
             'packages': ['ebcli'],
-        }
+            }
     }
-    setup_options['console'] = ['bin\eb']
+    setup_options['console'] = ['bin/eb']
 
 setup(**setup_options)
+
+if 'py2exe' in sys.argv:
+    # After py2exe is done we need to import all the data files botocore
+    # relies on
+    import shutil
+    import os
+    from subprocess import Popen, PIPE
+
+    def run(cmd):
+        sys.stdout.write("Running cmd: %s\n" % cmd)
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, env=os.environ)
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            raise Exception("Bad rc (%d): %s" % (p.returncode, stdout + stderr))
+        return stdout + stderr
+
+    def copy_data_directories():
+        # We need to move the .json files in awscli and botocore
+        # into the dist/ dir.
+        python = sys.executable
+        boto_data = run("""%s -c "from ebcli.bundled import botocore; import os; print(os.path.join(os.path.dirname(botocore.__file__), 'data'))" """ % python).strip()
+        print(boto_data)
+        shutil.copytree(boto_data, os.path.join('dist', 'ebcli', 'bundled', 'botocore', 'data'))
+
+    def copy_ca_cert():
+        # We need the cacert.pem from the requests library so we have
+        # to copy that in dist.
+        python = sys.executable
+        ca_cert = run("""%s -c "from ebcli.bundled.botocore.vendored import requests; import os; print(os.path.join(os.path.dirname(requests.__file__), 'cacert.pem'))" """ % python).strip()
+        shutil.copy(ca_cert, os.path.join('dist', 'ebcli', 'bundled', 'botocore', 'vendored', 'requests', 'cacert.pem'))
+
+    copy_data_directories()
+    copy_ca_cert()
