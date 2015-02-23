d91f476dc306d2c750745b878b877d4065f98861
Nick <humrichn@amazon.com>
2015-02-23 10:36:21 -0800

    Fix os_which to work for windows.
    Windows needs to have an .exe on the path.
    
    cr https://cr.amazon.com/r/3380542/

--- ebcli/core/fileoperations.py
+++ ebcli/core/fileoperations.py
@@ -317,6 +317,9 @@ def os_which(program):
     path = os.getenv('PATH')
     for p in path.split(os.path.pathsep):
         p = os.path.join(p, program)
+        if sys.platform.startswith('win'):
+            # Add .exe for windows
+            p += '.exe'
         if os.path.exists(p) and os.access(p, os.X_OK):
             return p
 
