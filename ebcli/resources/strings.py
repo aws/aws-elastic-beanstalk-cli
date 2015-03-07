ba838ca93ab1b841fb9394fe3f6da945724f7a08
Nick <humrichn@amazon.com>
2015-03-06 22:36:42 -0800

    Get rid of period after (Safe to Ctrl+C)
    cr https://cr.amazon.com/r/3435779/

--- ebcli/resources/strings.py
+++ ebcli/resources/strings.py
@@ -122,7 +122,7 @@ strings = {
     'sc.unstagedchanges': 'You have uncommitted changes.',
     'sc.gitnotinstalled': 'Your project is using git, but git doesn\'t appear to be installed.\n'
                           'Have you added git to your PATH?',
-    'events.streamprompt': ' -- Events -- (safe to Ctrl+C).',
+    'events.streamprompt': ' -- Events -- (safe to Ctrl+C)',
     'events.abortmessage': ' Use "eb abort" to abort command.',
 }
 
