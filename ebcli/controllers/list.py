52c9cb61dfedbc8b90abb2db1acdae03d94cd77c
Rahul Rajaram <rahuraja@amazon.com>
2017-10-19 20:24:39 -0700

    Rectify the usage text of 'eb list'.
    It presently shows 'environment_name' as a required positional argument, however, it doesn't accept any positional arguments. Customers will see the EBCLI raise if such an argument is provided.
    
    SIM: https://sim.amazon.com/issues/BEANSTALK-8974
    cr: https://code.amazon.com/reviews/CR-814741

--- ebcli/controllers/list.py
+++ ebcli/controllers/list.py
@@ -20,7 +20,7 @@ class ListController(AbstractBaseController):
     class Meta:
         label = 'list'
         description = strings['list.info']
-        usage = AbstractBaseController.Meta.usage.replace('{cmd}', label)
+        usage = 'eb list [options ...]'
         arguments = [
             (['-a', '--all'], dict(action='store_true',
                                    help=flag_text['list.all']))
