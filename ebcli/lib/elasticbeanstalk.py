aws/aws-elastic-beanstalk-cli

commit 1733d5ab95076fb87086d36577abe7c4cc96288b

Rename 'commonops.get_all_env_names' --> 'elasticbeanstalk.get_all_environment_names'
SIM: https://sim.amazon.com/issues/BEANSTALK-15367

cr https://code.amazon.com/reviews/CR-2007926

ebcli/lib/elasticbeanstalk.py
@@ -455,6 +455,12 @@ def get_app_environments(app_name, include_deleted=False, deleted_back_to=None):
     return Environment.json_to_environment_objects_array(result['Environments'])
 
 
+def get_all_envronment_names():
+    environments = elasticbeanstalk.get_all_environments()
+
+    return [environment.name for environment in environments]
+
+
 def get_all_environments():
     LOG.debug('Inside get_all_environments api wrapper')
     result = _make_api_call('describe_environments',

ebcli/controllers/create.py
@@ -433,7 +433,7 @@ def get_unique_environment_name(app_name):
     :return: A unique name for a new environment
     """
     default_name = app_name + '-dev'
-    current_environments = commonops.get_all_env_names()
+    current_environments = elasticbeanstalk.get_all_environment_names()
 
     return utils.get_unique_name(default_name, current_environments)
 

ebcli/operations/commonops.py
@@ -338,11 +338,6 @@ def get_env_event_string(event, long_format=False):
         return u'{0} - {1}: {2}'.format(environment.rjust(40), severity, message)
 
 
-def get_all_env_names():
-    envs = elasticbeanstalk.get_all_environments()
-    return [e.name for e in envs]
-
-
 def get_app_version_labels(app_name):
     app_versions = elasticbeanstalk.get_application_versions(app_name)['ApplicationVersions']
     return [v['VersionLabel'] for v in app_versions]

ebcli/operations/createops.py
@@ -257,7 +257,7 @@ def create_env(env_request, interactive=True):
                     cname = io.prompt_for_cname()
                 elif re.match(responses['env.nameexists'], e.message):
                     io.echo(strings['env.exists'])
-                    current_environments = commonops.get_all_env_names()
+                    current_environments = elasticbeanstalk.get_all_environment_names()
                     unique_name = utils.get_unique_name(env_request.env_name,
                                                         current_environments)
                     env_request.env_name = io.prompt_for_environment_name(

tests/unit/operations/test_environments.py
@@ -22,7 +22,7 @@ from ebcli.objects.requests import CreateEnvironmentRequest
 class TestCreateEnvironment(BaseOperationsTest):
     module_name = 'createops'
 
-    @patch('ebcli.operations.createops.commonops.get_all_env_names')
+    @patch('ebcli.operations.createops.elasticbeanstalk.get_all_environment_names')
     def test_create_new_environment_envname_taken(self, mock_names):
         mock_names.return_value = ['my-env', 'my-env2']
         self.mock_elasticbeanstalk.create_environment.side_effect = [

commit 1482ed8c43f76e9e1c47396b24db242800e4039a

Fix typo in method name
This typo was introduced in 1733d5ab95076fb87086d36577abe7c4cc96288b

SIM: https://sim.amazon.com/issues/BEANSTALK-15471

ebcli/lib/elasticbeanstalk.py
@@ -461,8 +461,8 @@ def get_app_environments(app_name, include_deleted=False, deleted_back_to=None):
     return Environment.json_to_environment_objects_array(result['Environments'])
 
 
-def get_all_envronment_names():
-    environments = elasticbeanstalk.get_all_environments()
+def get_all_environment_names():
+    environments = get_all_environments()
 
     return [environment.name for environment in environments]
 