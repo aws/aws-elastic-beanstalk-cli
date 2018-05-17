81715c2ea116904e5ddd8857681622de4865372a
Rahul Rajaram <rahuraja@amazon.com>
2018-05-17 12:32:04 -0700

    Fix 'eb appversion' controller tests
    SIM: https://sim.amazon.com/issues/BEANSTALK-15503

--- tests/unit/controllers/test_appversion.py
+++ tests/unit/controllers/test_appversion.py
@@ -10,59 +10,69 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-
-import unittest
+import os
+import shutil
 
 import mock
+from pytest_socket import disable_socket, enable_socket
+import unittest
 
 from ebcli.core import fileoperations
 from ebcli.core.ebcore import EB
 from ebcli.objects.solutionstack import SolutionStack
-from tests.unit.controllers.basecontrollertest import BaseControllerTest
 
 
-@unittest.skip
-class TestAppVersions(BaseControllerTest):
+class TestAppVersions(unittest.TestCase):
     app_name = 'awesomeApp'
-    solution = SolutionStack('64bit Amazon Linux 2014.03 '
-                             'v1.0.6 running PHP 5.5')
+    solution = '64bit Amazon Linux 2014.03 v1.0.6 running PHP 5.5'
 
     def setUp(self):
-        self.module_name = 'appversion'
-        super(TestAppVersions, self).setUp()
-        fileoperations.create_config_file(self.app_name, 'us-west-2',
-                                          self.solution.name)
+        disable_socket()
+        self.root_dir = os.getcwd()
+        if not os.path.exists('testDir'):
+            os.mkdir('testDir')
 
-        self.patcher_versions_ops = mock.patch('ebcli.controllers.appversion.appversionops')
-        self.patcher_gitops = mock.patch('ebcli.operations.gitops')
-        self.mock_versions_ops = self.patcher_versions_ops.start()
-        self.mock_gitops = self.patcher_gitops.start()
+        os.chdir('testDir')
+
+        fileoperations.create_config_file(
+            self.app_name,
+            'us-west-2',
+            self.solution
+        )
 
     def tearDown(self):
-        self.patcher_versions_ops.stop()
-        self.patcher_gitops.stop()
-        super(TestAppVersions, self).tearDown()
+        os.chdir(self.root_dir)
+        shutil.rmtree('testDir')
 
-    def test_delete_with_version_label(self):
-        version_label = 'V1'
+        enable_socket()
 
+    @mock.patch('ebcli.controllers.appversion.appversionops')
+    def test_delete_particular_version_label(
+            self,
+            appversionops_mock
+    ):
         EB.Meta.exit_on_close = False
-        self.app = EB(argv=['appversion', '--delete', version_label])
+        self.app = EB(argv=['appversion', '--delete', 'version-label-1'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
-        self.mock_versions_ops.delete_app_version_label.assert_called_with(self.app_name, version_label)
+        appversionops_mock.delete_app_version_label.assert_called_with(self.app_name, 'version-label-1')
 
-    @mock.patch('ebcli.controllers.appversion.elasticbeanstalk')
-    def test_enter_interactive_mode(self, mock_elasticbeanstalk):
+    @mock.patch('ebcli.controllers.appversion.appversionops')
+    @mock.patch('ebcli.controllers.appversion.elasticbeanstalk.get_application_versions')
+    def test_enter_interactive_mode(
+            self,
+            get_application_versions_mock,
+            appversionops_mock
+    ):
         app_versions = [
             {u'ApplicationName': self.app_name, u'VersionLabel': 'v13'},
             {u'ApplicationName': self.app_name, u'VersionLabel': 'v8'}
         ]
         get_app_versions_response = {u'ApplicationVersions': app_versions}
 
-        mock_elasticbeanstalk.get_application_versions.return_value = get_app_versions_response
+        get_application_versions_mock.return_value = get_app_versions_response
 
         EB.Meta.exit_on_close = False
         self.app = EB(argv=['appversion'])
@@ -70,4 +80,4 @@ class TestAppVersions(BaseControllerTest):
         self.app.run()
         self.app.close()
 
-        self.mock_versions_ops.display_versions.assert_called_with(self.app_name, None, app_versions)
+        appversionops_mock.display_versions.assert_called_with(self.app_name, None, app_versions)
