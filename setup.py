34ea1c702e207578bae9a06c04947cee3962fe96
Rahul Rajaram <rahuraja@amazon.com>
2018-06-26 18:32:50 -0700

    Remove py2exe code from setup.py and remove dependency on pyyaml
    It is unclear as to why the check for 'py2exe' in the command line
    arguments was ever added. The usage of the EBCLI with 'py2exe' is not
    supported or documented.
    
    Furthermore, 'py2exe' supports only Python 3.3 and Python 3.4 but not
    Python 2.7, Python 3.5, or Python 3.6, and has not been under active
    development since October 2014. It is also classified as a '4- Beta'
    project. As such, it would be a big risk to depend on it.
    
    1. So, this commit removes the 'py2exe' code in 'setup.py'
    
    2. This commit also removes the file, 'generate_msi.py', which is ommitted
    from the packaged version of the the awsebcli and as such is useless to
    customers and useless in the process of EBCLI development. 'generate_msi.py'
    was originally added in the commit dd6164d1ce707029eb6045d6025e0587157046d5
    and as part of the review: https://cr.amazon.com/r/3386526/
    
    3. Since none of the remaining code in the EBCLI uses 'pyyaml', this
    commit eliminates it as a dependency.
    
    py2exe PyPi repository: https://pypi.org/project/py2exe/#history
    
    SIM: https://sim.amazon.com/issues/BEANSTALK-16007
    
    cr https://code.amazon.com/reviews/CR-2432116

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ requires = [
     'colorama>=0.3.9,<0.4.0',  # use the same range that 'docker-compose' uses
     'pathspec==0.5.5',
     'python-dateutil>=2.1,<3.0.0',  # use the same range that 'botocore' uses
-    'pyyaml>=3.11',
     'setuptools >= 20.0',
     'docker-compose >= 1.21.2, < 1.22.0',
     'semantic_version == 2.5.0',
@@ -79,6 +78,7 @@ setup_options = dict(
     },
 )
 
+
 def _unpack_eggs(egg_list):
     import os
     for pkg in egg_list:
@@ -86,68 +86,10 @@ def _unpack_eggs(egg_list):
         eggs = pkg_resources.require(pkg)
         from setuptools.archive_util import unpack_archive
         for egg in eggs:
-           if os.path.isdir(egg.location):
-               sys.path.insert(0, egg.location)
-               continue
-           unpack_archive(egg.location, os.path.abspath(os.path.dirname(egg.location)))
+            if os.path.isdir(egg.location):
+                sys.path.insert(0, egg.location)
+                continue
+            unpack_archive(egg.location, os.path.abspath(os.path.dirname(egg.location)))
 
-
-if 'py2exe' in sys.argv:
-    data_files = setup_options['package_data']
-    # This will actually give us a py2exe command.
-    import py2exe
-    import cement.ext
-    import pkgutil
-    import encodings
-    # We need to manually include all cement.ext modules since py2exe doesnt
-    # pull them in.
-    _unpack_eggs(['jmespath', 'python-dateutil', 'pyyaml'])
-    includes = []
-    for importer, modname, ispkg in pkgutil.iter_modules(cement.ext.__path__):
-        includes.append('cement.ext.' + modname)
-    # And we have some py2exe specific options.
-    setup_options['options'] = {
-        'py2exe': {
-            'includes': ['encodings'] + includes,
-            'excludes': ['Tkinter', 'tcl'],
-            'optimize': 0,
-            'skip_archive': True,
-            'packages': ['ebcli'],
-            }
-    }
-    setup_options['console'] = ['bin/eb', 'bin/ebp']
 
 setup(**setup_options)
-
-if 'py2exe' in sys.argv:
-    # After py2exe is done we need to import all the data files botocore
-    # relies on
-    import shutil
-    import os
-    from subprocess import Popen, PIPE
-
-    def run(cmd):
-        sys.stdout.write("Running cmd: %s\n" % cmd)
-        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, env=os.environ)
-        stdout, stderr = p.communicate()
-        if p.returncode != 0:
-            raise Exception("Bad rc (%d): %s" % (p.returncode, stdout + stderr))
-        return stdout + stderr
-
-    def copy_data_directories():
-        # We need to move the .json files in awscli and botocore
-        # into the dist/ dir.
-        python = sys.executable
-        boto_data = run("""%s -c "from ebcli.bundled import botocore; import os; print(os.path.join(os.path.dirname(botocore.__file__), 'data'))" """ % python).strip()
-        print(boto_data)
-        shutil.copytree(boto_data, os.path.join('dist', 'ebcli', 'bundled', 'botocore', 'data'))
-
-    def copy_ca_cert():
-        # We need the cacert.pem from the requests library so we have
-        # to copy that in dist.
-        python = sys.executable
-        ca_cert = run("""%s -c "from ebcli.bundled.botocore.vendored import requests; import os; print(os.path.join(os.path.dirname(requests.__file__), 'cacert.pem'))" """ % python).strip()
-        shutil.copy(ca_cert, os.path.join('dist', 'ebcli', 'bundled', 'botocore', 'vendored', 'requests', 'cacert.pem'))
-
-    copy_data_directories()
-    copy_ca_cert()
