43b9aeceb77a5d853d2a304b1d6cdca488c13d1d
Rahul Rajaram <rahuraja@amazon.com>
2018-07-05 23:31:59 -0700

    Update unittest coverage for saved_configs.py
    SIM: https://sim.amazon.com/issues/BEANSTALK-16129
    CR: https://code.amazon.com/reviews/CR-2537805

--- tests/unit/operations/test_saved_configs.py
+++ tests/unit/operations/test_saved_configs.py
@@ -13,9 +13,11 @@
 
 import os
 import shutil
-import unittest
 import sys
+
 import mock
+from pytest_socket import enable_socket, disable_socket
+import unittest
 
 from ebcli.core import fileoperations
 from ebcli.operations import saved_configs
@@ -27,8 +29,8 @@ class TestResolveConfigLocations(unittest.TestCase):
     platform = 'python'
     cfg_name = 'myfile.cfg.yml'
 
-
     def setUp(self):
+        disable_socket()
         # set up test directory
         if not os.path.exists('testDir'):
             os.makedirs('testDir/.elasticbeanstalk/')
@@ -43,8 +45,8 @@ class TestResolveConfigLocations(unittest.TestCase):
         else:
             self.data = ''
 
-
     def tearDown(self):
+        disable_socket()
         os.chdir(os.path.pardir)
         if os.path.exists('testDir'):
             if sys.platform.startswith('win'):
@@ -203,4 +205,136 @@ class TestResolveConfigLocations(unittest.TestCase):
         mock_filename.assert_called_with(self.cfg_name)
         mock_validate.assert_called_with(self.app_name, self.cfg_name)
 
+    @mock.patch('ebcli.operations.saved_configs.download_config_from_s3')
+    @mock.patch('ebcli.operations.saved_configs.elasticbeanstalk.create_configuration_template')
+    def test_create_config(
+            self,
+            create_configuration_template_mock,
+            download_config_from_s3_mock
+    ):
+        saved_configs.create_config(
+            'my-application',
+            'environment-1',
+            'config-template'
+        )
+        download_config_from_s3_mock.assert_called_once_with('my-application', 'config-template')
+        create_configuration_template_mock.assert_called_once_with(
+            'my-application',
+            'environment-1',
+            'config-template',
+            'Configuration created from the EB CLI using "eb config save".'
+        )
 
+    @mock.patch('ebcli.operations.saved_configs.commonops.update_environment')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.env_yaml_exists')
+    def test_update_environment_with_config_file(
+            self,
+            env_yaml_exists_mock,
+            update_environment_mock
+    ):
+        env_yaml_exists_mock.return_value = True
+        saved_configs.update_environment_with_config_file(
+            'environment-1',
+            'cfg-name',
+            False
+        )
+        update_environment_mock.assert_called_once_with(
+            'environment-1',
+            None,
+            False,
+            template='cfg-name',
+            timeout=None
+        )
+
+    @mock.patch('ebcli.operations.saved_configs.commonops.update_environment')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.env_yaml_exists')
+    def test_update_environment_with_config_file(
+            self,
+            env_yaml_exists_mock,
+            update_environment_mock
+    ):
+        env_yaml_exists_mock.return_value = True
+        saved_configs.update_environment_with_config_data(
+            'environment-1',
+            'cfg-name',
+            False
+        )
+        update_environment_mock.assert_called_once_with(
+            'environment-1',
+            None,
+            False,
+            template_body='cfg-name',
+            timeout=None
+        )
+
+    @mock.patch('ebcli.operations.saved_configs.elasticbeanstalk.get_storage_location')
+    @mock.patch('ebcli.operations.saved_configs.s3.get_object')
+    @mock.patch('ebcli.operations.saved_configs.write_to_local_config')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.set_user_only_permissions')
+    @mock.patch('ebcli.operations.saved_configs._get_s3_keyname_for_template')
+    def test_download_config_from_s3(
+            self,
+            get_s3_keyname_for_template_mock,
+            set_user_only_permissions_mock,
+            write_to_local_config_mock,
+            get_object_mock,
+            get_storage_location_mock
+    ):
+        get_s3_keyname_for_template_mock.return_value = 'template'
+        write_to_local_config_mock.return_value = 'file_path'
+        get_storage_location_mock.return_value = 'bucket'
+        get_object_mock.return_value = 'body'
+
+        saved_configs.download_config_from_s3('my-application', 'cfg-name')
+
+        get_storage_location_mock.assert_called_once_with()
+        get_object_mock.assert_called_once_with('bucket', 'template')
+        write_to_local_config_mock.assert_called_once_with('cfg-name', 'body')
+        set_user_only_permissions_mock.assert_called_once_with('file_path')
+
+    @mock.patch('ebcli.operations.saved_configs.elasticbeanstalk.delete_configuration_template')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.delete_file')
+    @mock.patch('ebcli.operations.saved_configs.resolve_config_location')
+    def test_delete_config(
+            self,
+            resolve_config_location_mock,
+            delete_file_mock,
+            delete_configuration_template_mock
+    ):
+        resolve_config_location_mock.return_value = 'location'
+
+        saved_configs.delete_config('my-application', 'cfg-name')
+
+        delete_configuration_template_mock.assert_called_once_with('my-application', 'cfg-name')
+        delete_file_mock.assert_called_once_with('location')
+
+    @mock.patch('ebcli.operations.saved_configs.upload_config_file')
+    @mock.patch('ebcli.operations.saved_configs.resolve_config_location')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.get_filename_without_extension')
+    def test_resolve_config_name(
+            self,
+            get_filename_without_extension_mock,
+            resolve_config_location_mock,
+            upload_config_file_mock
+    ):
+        resolve_config_location_mock.return_value = 'config_location'
+        get_filename_without_extension_mock.return_value = 'file_name'
+
+        saved_configs.resolve_config_name('my-application', 'cfg-name')
+
+        upload_config_file_mock.assert_called_once_with('my-application', 'file_name', 'config_location')
+
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.make_eb_dir')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.write_to_eb_data_file')
+    @mock.patch('ebcli.operations.saved_configs.fileoperations.get_eb_file_full_location')
+    def test_write_to_local_config(
+            self,
+            get_eb_file_full_location_mock,
+            write_to_eb_data_file_mock,
+            make_eb_dir_mock
+    ):
+        saved_configs.write_to_local_config('cfg-name', 'data')
+
+        make_eb_dir_mock.assert_called_once_with('saved_configs{}'.format(os.path.sep))
+        write_to_eb_data_file_mock.assert_called_once_with('saved_configs{}cfg-name.cfg.yml'.format(os.path.sep), 'data')
+        get_eb_file_full_location_mock.assert_called_once_with('saved_configs{}cfg-name.cfg.yml'.format(os.path.sep))
