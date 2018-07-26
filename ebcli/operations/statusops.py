2fd74179969ca8a0dcb63a9bd61475f5dd505f9f
Rahul Rajaram <rahuraja@amazon.com>
2018-07-25 18:47:53 -0700

    Simplify statusops.py
    SIM: https://sim.amazon.com/issues/BEANSTALK-16277
    
    cr https://code.amazon.com/reviews/CR-2704692

--- ebcli/operations/statusops.py
+++ ebcli/operations/statusops.py
@@ -12,12 +12,10 @@
 # language governing permissions and limitations under the License.
 import six
 
-from ..lib import elasticbeanstalk, elb, elbv2
-from ..core import io
-from ..resources.strings import alerts
-from ..resources.statics import elb_names
-from ..objects.exceptions import NotFoundError
-from . import commonops, gitops, solution_stack_ops
+from ebcli.lib import elasticbeanstalk, elb, elbv2
+from ebcli.core import io
+from ebcli.resources.strings import alerts
+from ebcli.operations import gitops, solution_stack_ops
 
 
 SPACER = ' ' * 5
@@ -31,69 +29,69 @@ def status(app_name, env_name, verbose):
         elasticbeanstalk.get_environment_resources,
         health=True
     )
+    env_dict = elasticbeanstalk.get_environment_resources(env_name)
+    _print_information_about_elb_and_instances(env_dict) if verbose else None
+    _alert_if_platform_is_older_than_the_latest(env)
+    _print_codecommit_repositories()
 
-    if verbose:
-        # Print number of running instances
-        env_dict = elasticbeanstalk.get_environment_resources(env_name)
-        instances = [i['Id'] for i in
-                     env_dict['EnvironmentResources']['Instances']]
-        io.echo('  Running instances:', len(instances))
 
-        try:
-            load_balancer_name = [i['Name'] for i in
-                                  env_dict['EnvironmentResources']['LoadBalancers']][0]
-            if elb.version(load_balancer_name) == elb_names.APPLICATION_VERSION:
-                target_groups = [t['TargetGroupArn'] for t in elbv2.get_target_groups_for_load_balancer(load_balancer_name)]
-                target_group_states = elbv2.get_target_group_healths(target_groups)
-
-                for target_group_arn, target_group_health in six.iteritems(target_group_states):
-                    current_target_group_instances = []
-                    for target_group_description in target_group_health['TargetHealthDescriptions']:
-
-                        current_target_group_instances.append(target_group_description['Target']['Id'])
-                        target_health = target_group_description['TargetHealth']
-
-                        io.echo(SPACER * 2, target_group_description['Target']['Id'] + ': ' + target_health['State'])
-
-                        if 'Description' in target_health and len(target_health['Description']) > 0:
-                            io.echo(SPACER * 3,
-                                    'Description: ' + target_health['Description'])
-                        if 'Reason' in target_health and len(target_health['Reason']) > 0:
-                            io.echo(SPACER * 3,
-                                    'Reason: ' + target_health['Reason'])
-
-                    for i in instances:
-                        if i not in current_target_group_instances:
-                            io.echo(SPACER * 2, i + ':', 'N/A (Not registered '
-                                                    'with Target Group)')
-            elif load_balancer_name:
-                instance_states = elb.get_health_of_instances(load_balancer_name)
-                for i in instance_states:
-                    instance_id = i['InstanceId']
-                    state = i['State']
-                    descrip = i['Description']
-                    if state == 'Unknown':
-                        state += '(' + descrip + ')'
-                    io.echo(SPACER, instance_id + ':', state)
-                for i in instances:
-                    if i not in [x['InstanceId'] for x in instance_states]:
-                        io.echo(SPACER, i + ':', 'N/A (Not registered '
-                                                'with Load Balancer)')
-
-        except (IndexError, KeyError, NotFoundError) as e:
-            #No load balancer. Dont show instance status
-            pass
-
+def _alert_if_platform_is_older_than_the_latest(env):
     latest = solution_stack_ops.find_solution_stack_from_string(env.platform.name, find_newer=True)
-
     if env.platform != latest:
         io.log_alert(alerts['platform.old'])
 
-    # print out code commit repos if they exist
+
+def _print_codecommit_repositories():
     default_branch = gitops.get_default_branch()
     default_repo = gitops.get_default_repository()
-    codecommit_setup = (default_repo and default_repo is not None) or (default_branch and default_branch is not None)
-    if codecommit_setup:
+    if default_repo and default_branch:
         io.echo("Current CodeCommit settings:")
         io.echo("  Repository: " + str(default_repo))
         io.echo("  Branch: " + str(default_branch))
+
+
+def _print_information_about_elb_and_instances(env_dict):
+    instances = [instance['Id'] for instance in env_dict['EnvironmentResources']['Instances']]
+    io.echo('  Running instances:', len(instances))
+
+    load_balancers = env_dict['EnvironmentResources']['LoadBalancers']
+    if load_balancers:
+        load_balancer_name = load_balancers[0]['Name']
+        if elb.is_classic_load_balancer(load_balancer_name):
+            _print_elb_health_stats(load_balancer_name, instances)
+        elif load_balancer_name:
+            _print_elbv2_health_stats(load_balancer_name, instances)
+
+
+def _print_elbv2_health_stats(load_balancer_name, instances):
+    target_groups = [t['TargetGroupArn'] for t in elbv2.get_target_groups_for_load_balancer(load_balancer_name)]
+    target_group_states = elbv2.get_target_group_healths(target_groups)
+
+    for target_group_arn, target_group_health in six.iteritems(target_group_states):
+        current_target_group_instances = []
+        for target_group_description in target_group_health['TargetHealthDescriptions']:
+            current_target_group_instances.append(target_group_description['Target']['Id'])
+            target_health = target_group_description['TargetHealth']
+
+            io.echo(SPACER * 2, target_group_description['Target']['Id'] + ': ' + target_health['State'])
+
+            if 'Description' in target_health and len(target_health['Description']) > 0:
+                io.echo(SPACER * 3, 'Description: ' + target_health['Description'])
+            if 'Reason' in target_health and len(target_health['Reason']) > 0:
+                io.echo(SPACER * 3, 'Reason: ' + target_health['Reason'])
+
+        for i in set(instances) - set(current_target_group_instances):
+            io.echo(SPACER * 2, i + ':', 'N/A (Not registered with Target Group)')
+
+
+def _print_elb_health_stats(load_balancer_name, instances):
+    instance_states = elb.get_health_of_instances(load_balancer_name)
+    for i in instance_states:
+        instance_id = i['InstanceId']
+        state = i['State']
+        description = i['Description']
+        if state == 'Unknown':
+            state += '(' + description + ')'
+        io.echo(SPACER, instance_id + ':', state)
+    for i in set(instances) - set([x['InstanceId'] for x in instance_states]):
+        io.echo(SPACER, i + ':', 'N/A (Not registered with Load Balancer)')
