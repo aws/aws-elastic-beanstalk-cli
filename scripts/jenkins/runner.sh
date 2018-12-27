aws/aws-elastic-beanstalk-cli

commit 44e2ef4e9fd7e797db1d7b148d2f5159e8cdbab9

Fix typos in runner scripts
SIM: https://sim.amazon.com/issues/BEANSTALK-17678

cr https://code.amazon.com/reviews/CR-4259604

scripts/jenkins/runner.sh
@@ -2,6 +2,7 @@
 
 ARTIFACTS_DIRECTORY="$HOME/awsebcli_artifacts"
 GIT_COMMIT=`git rev-parse HEAD`
+
 if [[ ! $GIT_BRANCH ]]; then
     GIT_BRANCH = `git rev-parse --abbrev-ref HEAD`
 fi
@@ -9,17 +10,23 @@ PYTHON_INSTALLATION=$1
 PYTHON_VERSION=`${PYTHON_INSTALLATION} -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'`
 VENV_ENV_NAME="$PYTHON_VERSION-$GIT_COMMIT"
 USAGE_TEXT=$(cat <<usage
+This Bash script runs the unit tests of the current Git commit in an exclusive
+virtualenv created from the Python installation described by the first argument
+to it. If the current branch is a `master` branch (irrespective of the `remote`),
+the script generates an EBCLI artifact tar file available for consumers such as an
+end-to-end tests runner, and for the purposes of uploading to PyPi.
+
 Usage:
 
-    bash runner.py <PYTHON_EXECUTABLE>
+    ./scripts/jenkins/runner.sh <PYTHON_EXECUTABLE>
 
 e.g., to test this project against Python 2.7 and Python 3.6, you would:
 
-    python runner.py /usr/local/bin/python2.7
+    ../scripts/jenkins/runner.sh /usr/local/bin/python2.7
 
         and
 
-    python runner.py /usr/local/bin/python3.6
+    ./scripts/jenkins/runner.sh /usr/local/bin/python3.6
 
 usage
 )
@@ -112,10 +119,10 @@ step_title "Verifying Python binary path is valid"
 validate_python_version_name
 increment_step_number
 
-step_title "Ensure AWSEBCLI installs correctly after AWSCLI"
+step_title "Ensuring AWSEBCLI installs correctly after AWSCLI"
 ensure_awsebcli_installs_correctly_after_awscli()
 {
-    substep_title "Create new Python $PYTHON_VERSION virtualenv"
+    substep_title "Creating new Python $PYTHON_VERSION virtualenv"
     virtualenv -p $PYTHON_INSTALLATION $VENV_ENV_NAME
     exit_upon_substep_failure
 
@@ -142,10 +149,10 @@ ensure_awsebcli_installs_correctly_after_awscli()
 ensure_awsebcli_installs_correctly_after_awscli
 increment_step_number
 
-step_title "Ensure AWSCLI installs correctly after AWSEBCLI"
+step_title "Ensuring AWSCLI installs correctly after AWSEBCLI"
 ensure_awscli_installs_correctly_after_awsebcli()
 {
-    substep_title "Create new Python $PYTHON_VERSION virtualenv"
+    substep_title "Creating new Python $PYTHON_VERSION virtualenv"
     virtualenv -p $PYTHON_INSTALLATION $VENV_ENV_NAME
     exit_upon_substep_failure
 
@@ -176,7 +183,7 @@ ensure_awscli_installs_correctly_after_awsebcli()
 ensure_awscli_installs_correctly_after_awsebcli
 increment_step_number
 
-step_title "Create new Python $PYTHON_VERSION virtualenv"
+step_title "Creating new Python $PYTHON_VERSION virtualenv"
 virtualenv -p $PYTHON_INSTALLATION $VENV_ENV_NAME
 exit_upon_failure
 

scripts/jenkins/runner.ps1
@@ -1,26 +1,26 @@
 <#
 .DESCRIPTION
-Script runs the unit tests of the current Git commit in an exclusive virtualenv created
-from the the PYTHON_INSTALLATION path passed. If the current branch is a `master` branch
-(irrespective of the `remote`), the script generates an EBCLI artifact tar file available
-for consumers such as an end-to-end tests runner, and for the purposes of uploading to
-PyPi.
+This PowerShell script runs the unit tests of the current Git commit in an exclusive
+virtualenv created from the the PYTHON_INSTALLATION path passed. If the current branch
+is a `master` branch (irrespective of the `remote`), the script generates an EBCLI
+artifact tar file available for consumers such as an end-to-end tests runner, and for
+the purposes of uploading to PyPi.
 
 .EXAMPLE
 Usage:
 
-    bash runner.py <PYTHON_EXECUTABLE>
+    .\scripts\jenkins\runner <PYTHON_EXECUTABLE>
 
 e.g., to test this project against Python 2.7 and Python 3.6, you would:
 
-    python runner.py C:\Python27\python.exe
+    .\scripts\jenkins\runner C:\Python27\python.exe
 
         and
 
-    python runner.py C:\Python36\python.exe
+    .\scripts\jenkins\runner C:\Python36\python.exe
 
 .SYNOPSIS
-EBCLI test runner + artifact generation tool
+EBCLI test suite runner + artifact generation tool
 #>
 Param(
     [Parameter(Mandatory=$True,Position=1)]
@@ -89,7 +89,7 @@ Print-StepTitle "Verifying Python binary path is valid"
 Validate-PythonVersionName
 Exit-UponFailure
 
-Print-StepTitle "Ensure AWSEBCLI installs correctly after AWSCLI"
+Print-StepTitle "Ensuring AWSEBCLI installs correctly after AWSCLI"
 function Ensure-AWSEBCLIInstallsCorrectlyAfterAWSCLI()
 {
     Print-SubStepTitle "Create new Python $PYTHON_VERSION virtualenv"
@@ -119,7 +119,7 @@ function Ensure-AWSEBCLIInstallsCorrectlyAfterAWSCLI()
 }
 Ensure-AWSEBCLIInstallsCorrectlyAfterAWSCLI
 
-Print-StepTitle "Ensure AWSCLI installs correctly after AWSEBCLI"
+Print-StepTitle "Ensuring AWSCLI installs correctly after AWSEBCLI"
 function Ensure-AWSCLIInstallsCorrectlyAfterAWSEBCLI()
 {
     Print-SubStepTitle "Create new Python $PYTHON_VERSION virtualenv"
@@ -188,7 +188,6 @@ if ( $env:GIT_BRANCH -like '*master' ) {
     Increment-SubStepNumber
 
     Print-SubStepTitle "Packaging awsebcli to store in $ARTIFACTS_DIRECTORY\$PYTHON_VERSION"
-    Write-Output "******************************************************"
     Invoke-Expression "python setup.py sdist --dist-dir '$ARTIFACTS_DIRECTORY\$PYTHON_VERSION'"
     Increment-SubStepNumber
 } else {

commit a24d734e313359e7b09bb0710a06ece2693e9ee0

Fix typo introduced in 44e2ef4e
CR: https://code.amazon.com/reviews/CR-4259604

scripts/jenkins/runner.sh
@@ -33,7 +33,7 @@ Usage:
 
 e.g., to test this project against Python 2.7 and Python 3.6, you would:
 
-    ../scripts/jenkins/runner.sh /usr/local/bin/python2.7
+    ./scripts/jenkins/runner.sh /usr/local/bin/python2.7
 
         and
 