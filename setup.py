8d579f7c5775250855e897dc1f67b877bd8d0931
Rahul Rajaram <rahuraja@amazon.com>
2019-01-04 17:53:42 -0800

    Update urllib3, docker-compose, botocore, six dependencies
    It has been brought up in
    https://forums.aws.amazon.com/thread.jspa?messageID=883931&#883931
    that urllib3 and requests (which is a dependency of docker-compose)
    that the awsebcli requires presently are vulnerable.
    
    Since we'd now line to depend on urllib3>=1.24.1,<1.25, we need a
    version of botocore which supports urllib3>=1.24.1, which is
    botocore>=1.12.29.
    
    pytest requires six>=1.10. We update the EBCLI's requirement on six less
    restrictively to >=1.11,<1.12 to accommodate certain future
    dependencies.
    
    SIM: https://sim.amazon.com/issues/BEANSTALK-17743
    
    cr https://code.amazon.com/reviews/CR-4319845

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import ebcli
 
 
 requires = [
-    'botocore>=1.12.1,<1.13',
+    'botocore>=1.12.29,<1.13',
     'cement==2.8.2',
     'colorama>=0.3.9,<0.4.0',  # use the same range that 'docker-compose' uses
     'pathspec==0.5.5',
@@ -27,9 +27,9 @@ requires = [
     'PyYAML>=3.10,<=3.13',  # use the same range that 'aws-cli' uses. This is also compatible with 'docker-compose'
     'setuptools >= 20.0',
     'semantic_version == 2.5.0',
-    'six == 1.11.0',
+    'six>=1.11.0,<1.12.0',
     'termcolor == 1.1.0',
-    'urllib3>=1.21.1,<1.23'
+    'urllib3>=1.24.1,<1.25'
 ]
 
 testing_requires = [
@@ -39,7 +39,7 @@ testing_requires = [
 ]
 
 if not sys.platform.startswith('win'):
-    requires.append('docker-compose >= 1.21.2, < 1.22.0')
+    requires.append('docker-compose >= 1.23.2, < 1.24.0')
     requires.append('blessed>=1.9.5')
 
 
