aws/aws-elastic-beanstalk-cli

commit 9754f9331bc659c70ec39a3e0b3048905fa308d3

Guard against absence of response 'Date' in AWS response headers
The EBCLI patches AWS responses that botocore receives so that the
ResponseMetadata includes a 'date' key. Some services do not include
a 'date' key in the response header, resulting in a KeyError in the
EBCLI.

This change enables the EBCLI to recognize when a date is absent in
the response header and to include one by itself

SIM: https://sim.amazon.com/issues/BEANSTALK-18053
CR: https://code.amazon.com/reviews/CR-5599303

ebcli/lib/botopatch.py
@@ -15,6 +15,8 @@
 Apply botocore fixes as monkey patches.
 This allows us to use any version of botocore.
 """
+import datetime
+import time
 
 from botocore import parsers
 
@@ -56,7 +58,13 @@ def fix_botocore_to_pass_response_date():
             # BEGIN PATCH
             # Here we inject the date
             parsed['ResponseMetadata']['date'] = (
-                response['headers']['date']
+                response['headers'].get(
+                    'date',
+                    datetime.datetime.fromtimestamp(
+                        time.time(),
+                        datetime.timezone.utc
+                    )
+                )
             )
             # END PATCH
         return parsed

commit d6f311df02db4520ac2da585c1a6b43bebd52985

Fix Python 2.7 EBCLI breakage
In 9754f9331bc659c70ec39a3e0b3048905fa308d3, a change was introduced
to protect against AWS responses that did not contain a 'date'. The
patched 'date' however relied on the 'timezone' function from the
'datetime' module, which is not defined in Python 2.7.

This fix reverts the change. The patch wasn't immediately necessary.

SIM: https://sim.amazon.com/issues/BEANSTALK-18149

cr https://code.amazon.com/reviews/CR-6003039

ebcli/__init__.py
@@ -11,4 +11,4 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-__version__ = '3.14.12'
+__version__ = '3.14.13'

ebcli/lib/botopatch.py
@@ -18,6 +18,7 @@ This allows us to use any version of botocore.
 import datetime
 import time
 
+from dateutil import tz
 from botocore import parsers
 
 
@@ -58,13 +59,7 @@ def fix_botocore_to_pass_response_date():
             # BEGIN PATCH
             # Here we inject the date
             parsed['ResponseMetadata']['date'] = (
-                response['headers'].get(
-                    'date',
-                    datetime.datetime.fromtimestamp(
-                        time.time(),
-                        datetime.timezone.utc
-                    )
-                )
+                response['headers']['date']
             )
             # END PATCH
         return parsed