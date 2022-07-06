029ec383c78252fedb680085da5140122c8a3a10
Mingran Peng <mingran@amazon.com>
2022-07-06 15:30:46 -0700

    Fix zipping permission bug
    cr: https://code.amazon.com/reviews/CR-72989492

--- ebcli/core/fileoperations.py
+++ ebcli/core/fileoperations.py
@@ -15,15 +15,17 @@ import codecs
 import glob
 import json
 import os
+import pathlib
 import shutil
 import stat
 import sys
-import zipfile
-import yaml
 import warnings
+import yaml
+import zipfile
 from pathspec import PathSpec
 
 from cement.utils.misc import minimal_logger
+from ebcli.core import fileoperations
 from ebcli.objects.buildconfiguration import BuildConfiguration
 from six import StringIO
 from yaml import safe_load, safe_dump
@@ -389,14 +391,19 @@ def delete_app_versions():
 def zip_append_archive(target_file, source_file):
     zip_source = zipfile.ZipFile(source_file, 'r', allowZip64=True)
     zip_target = zipfile.ZipFile(target_file, 'a', allowZip64=True)
+    # Because zipfile doesn't preserve file permission code, we have to extract and then zip up using shutil
+    tmp_folder_name = 'tmpAppendFolder'
     with warnings.catch_warnings():
         # Ignore UserWarning raised by zip module for zipping modules.
         warnings.simplefilter('ignore', category=UserWarning)
-        for filename in zip_source.namelist():
-            zf = zip_source.read(filename)
-            zip_target.writestr(filename, zf)
+        zip_source.extractall(tmp_folder_name)
+        zip_target.extractall(tmp_folder_name)
     zip_target.close()
     zip_source.close()
+    delete_file(target_file)
+    path_without_suffix = pathlib.Path(target_file).with_suffix('')
+    shutil.make_archive(path_without_suffix, 'zip', tmp_folder_name)
+    delete_directory(tmp_folder_name)
 
 
 def zip_up_folder(directory, location, ignore_list=None):
