d3ed22554339dd8cde0443f9ef8e8d213a372929
Rahul Rajaram <rahuraja@amazon.com>
2024-09-10 13:55:37 -0400

    Avoid checking for pip 21 in dependency checker unit test
    Assume existence of modern pip versions

--- tests/test_dependencies_mismatch.py
+++ tests/test_dependencies_mismatch.py
@@ -14,9 +14,6 @@ from pip import __version__ as pip_version
 from pkg_resources import get_distribution, parse_version
 
 
-if pip_version != '21.1':
-    raise RuntimeError('You need pip==21.1 to check for dependency incompatibilities.')
-
 try:
     from pip._internal.operations.check import (
         check_package_set,
@@ -41,7 +38,7 @@ def color_green(message):
 def collect_package_requirements_by_requirement_name(req_name, package_set):
     required_by = dict()
     for pkg_name, package in package_set.items():
-        for req in package.requires:
+        for req in package.dependencies:
             if req.name.lower() == req_name.lower():
                 required_by[pkg_name] = req.specifier
 
