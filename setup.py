af0b84c8a9b1fade7916516a38f8d54d95d22b86
Rahul Rajaram <rahuraja@amazon.com>
2024-09-10 23:26:39 -0400

    Declare support drop for Python < 3.8 and Python 2.7

--- setup.py
+++ setup.py
@@ -66,10 +66,12 @@ setup_options = dict(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ),
     entry_points={
         'console_scripts': [
